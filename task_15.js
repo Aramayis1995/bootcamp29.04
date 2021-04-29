95c024988a47a8c96138ce8dafe5bd0b81c9b8ba
Aramayis <mayissargsyan1995@gmail.com>
2021-04-29 19:32:23 +0400

    maximum and minimum lengths in array

---
+++ task_15.js
@@ -0,0 +1,15 @@
+function str(n) {
+  let arr = [n[0], n[0]];
+  for (i = 0; i < n.length; i++) {
+    if (arr[0].length < n[i].length) {
+      arr[0] = n[i];
+    }
+  }
+  for (i = 0; i < n.length; i++) {
+    if (arr[1].length > n[i].length) {
+      arr[1] = n[i];
+    }
+  }
+  console.log(arr[0].length + arr[1].length);
+}
+str(["wish", "ly", "understand", "longer", "unexpected", "heart"]);
