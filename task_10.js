4a3920e1da255c45853237542eaf09e50da30572
Aramayis <mayissargsyan1995@gmail.com>
2021-04-29 19:11:00 +0400

    fix

--- task_10.js
+++ task_10.js
@@ -1,8 +1,12 @@
 function number(n) {
   let arr = [0, 1];
+  if (n === 0) {
+    console.log(arr[0]);
+    return;
+  }
   for (i = 0; i < n - 2; i++) {
     arr.push(arr[i] + arr[i + 1]);
   }
   console.log(arr[arr.length - 1]);
 }
-number(7);
+number(5);
