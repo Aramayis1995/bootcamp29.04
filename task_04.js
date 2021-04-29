0c4b2e071f62f5beac9f929c956cf4e7e94e5959
Aramayis <mayissargsyan1995@gmail.com>
2021-04-29 16:16:04 +0400

    task_04:fix

--- task_04.js
+++ task_04.js
@@ -1,14 +1,20 @@
 function number(n) {
   if (n % 3 === 0 && n % 5 === 0 && n % 7 === 0) {
-    console.log("420 is a multiple of 3, 5 and 7.");
+    console.log(n, " is a multiple of 3, 5 and 7.");
+  } else if (n % 5 === 0 && n % 3 === 0) {
+    console.log(n, " is a multiple of 5 and 3.");
   } else if (n % 5 === 0 && n % 7 === 0) {
-    console.log("35 is a multiple of 5 and 7.");
+    console.log(n, " is a multiple of 5 and 7.");
   } else if (n % 3 === 0 && n % 7 === 0) {
-    console.log("21 is a multiple of 3 and 7");
+    console.log(n, " is a multiple of 3 and 7");
   } else if (n % 3 === 0) {
-    console.log("24 is a multiple of 3.");
+    console.log(n, " is a multiple of 3.");
+  } else if (n % 5 === 0) {
+    console.log(n, " is a multiple of 5.");
+  } else if (n % 7 === 0) {
+    console.log(n, " is a multiple of 7.");
   } else if (n % 3 !== 0 && n % 5 !== 0 && n % 7 !== 0) {
-    console.log("13 is not a multiple of 3, 5 or 7.");
+    console.log(n, " is not a multiple of 3, 5 or 7.");
   }
 }
-number(13);
+number(7);
