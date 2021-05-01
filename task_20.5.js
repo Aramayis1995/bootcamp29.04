6a72386e7f23dea2c4749546e7eed6d348d33b6c
Aramayis <mayissargsyan1995@gmail.com>
2021-05-01 18:33:14 +0400

    fix:task_20.5 good option

--- task_20.5.js
+++ task_20.5.js
@@ -3,36 +3,27 @@ function passwardValidity(pass) {
     console.log("Invalid");
     return;
   }
-  let counter = 0;
-  let j = 1;
-  let k = 1;
-  let n = 1;
-  let m = 1;
+  let counter = 1;
   for (i = 0; i < pass.length; i++) {
     if (+pass[i] * 1 === +pass[i]) {
-      counter += j;
-      j = 0;
+      counter *= 3;
       continue;
     }
     if (pass[i] === "@" || pass[i] === "#" || pass[i] === "$") {
-      counter += k;
-      k = 0;
+      counter *= 5;
       continue;
     }
     if (97 <= pass.charCodeAt(i) && pass.charCodeAt(i) <= 122) {
-      counter += n;
-      n = 0;
-      continue;
+      counter *= 7;
     }
     if (65 <= pass.charCodeAt(i) && pass.charCodeAt(i) <= 90) {
-      counter += m;
-      m = 0;
+      counter *= 11;
     }
   }
-  if (counter === 4) {
+  if (counter % (3 * 5 * 7 * 11) === 0) {
     console.log("valid");
   } else {
     console.log("invalid");
   }
 }
-passwardValidity("585Daaaaa$");
+passwardValidity("#Aa1234");
